d8528f7bb7f33453689f7fa9ca42dadbaf7821bf
ArielWangX <arielxiewang@gmail.com>
2023-06-13 13:24:05 +1000

    maintain format consistency

--- src/components/AccountButton/AccountButton.tsx
+++ src/components/AccountButton/AccountButton.tsx
@@ -5,11 +5,11 @@ import Button from '../Button';
 
 type InputProps = {
   text: string
-  background_color: string
-  text_color: string
+  backgroundColor: string
+  textColor: string
 }
 
-const AccountButton: FC<InputProps> = ({ text, background_color, text_color }) => (
+const AccountButton: FC<InputProps> = ({ text, backgroundColor: background_color, textColor: text_color }) => (
   <div className="w-full flex items-center justify-center">
     <Button
       className={` bg-[${background_color}] w-full border border-[#007AD3] rounded-[5px] py-[14px] font-bold text-base text-[1.125rem] text-[${text_color}] leadding-[1.375rem]`}
