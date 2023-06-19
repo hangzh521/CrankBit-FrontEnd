daad7ffc19fc59514005000f1f70c990c41a684c
Limin Chen <kathy@Williams-MacBook-Pro.local>
2023-06-19 18:00:29 +1000

    Popup

--- src/components/Modal/PopupPage.tsx
+++ src/components/Modal/PopupPage.tsx
@@ -1,12 +1,12 @@
-import { FC, useState } from 'react';
-import CheckRounded from '@mui/icons-material/CheckRounded';
-import IconButton from '@mui/material/IconButton';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import Container from '@/layouts/Container';
+import { FC, useState } from 'react'
+import CheckRounded from '@mui/icons-material/CheckRounded'
+import IconButton from '@mui/material/IconButton'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+import Container from '@/layouts/Container'
 
 const PopupPage: FC = () => {
   const [isOpen, setIsOpen] = useState(true)
-  const handleClose = ():void => {
+  const handleClose = (): void => {
     setIsOpen(false)
   }
   if (!isOpen) {
@@ -17,19 +17,12 @@ const PopupPage: FC = () => {
       <div className="flex justify-center items-center">
         <div className="border rounded-md w-96 h-80 bg-white">
           <div className="text-end">
-            <IconButton
-              onClick={handleClose}
-              className="text-black w-12 h-12"
-            >
-              <CloseRoundedIcon
-                className="text-black text-[32px]"
-              />
+            <IconButton onClick={handleClose} className="text-black w-12 h-12">
+              <CloseRoundedIcon className="text-black text-[32px]" />
             </IconButton>
           </div>
           <div className="flex justify-center">
-            <div
-              className="bg-green-600 w-20 h-20 flex justify-center rounded-full"
-            >
+            <div className="bg-green-600 w-20 h-20 flex justify-center rounded-full">
               <IconButton>
                 <CheckRounded className="text-white font-inner text-[64px]" />
               </IconButton>
