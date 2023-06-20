c9164b6b4455c5abecf817a19622493603a921f3
Limin Chen <kathy@Williams-MacBook-Pro.local>
2023-06-20 15:32:26 +1000

    revise PopupPage 2nd

--- src/components/Modal/PopupPage.tsx
+++ src/components/Modal/PopupPage.tsx
@@ -5,31 +5,29 @@ import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import Container from '@/layouts/Container'
 
 const PopupPage: FC = () => {
-  const [isOpen, setIsOpen] = useState(true)
-  const handleClose = (): void => {
-    setIsOpen(false)
-  }
+  const [isOpen, setIsOpen] = useState<boolean>(true)
+  const handleClose = (): void => {setIsOpen(false)}
   if (!isOpen) {
     return null
   }
   return (
     <Container>
-      <div className="flex justify-center items-center">
-        <div className="border rounded-md w-96 h-80 bg-white">
+      <div className="justify-center items-center">
+        <div className="border rounded-md w-96 h-80 bg-white max-auto text-center">
           <div className="text-end">
             <IconButton onClick={handleClose} className="text-black w-12 h-12">
-              <CloseRoundedIcon className="text-black text-[32px]" />
+              <CloseRoundedIcon className="text-[32px]" />
             </IconButton>
           </div>
           <div className="flex justify-center">
-            <div className="bg-green-600 w-20 h-20 flex justify-center rounded-full">
+            <div className="bg-[#0EA739] w-20 h-20 rounded-full">
               <IconButton>
-                <CheckRounded className="text-white font-inner text-[64px]" />
+                <CheckRounded className="text-white text-[64px]" />
               </IconButton>
             </div>
           </div>
-          <div className="text-center font-bold text-xl mt-6 mb-16">
-            <p> Sign Up Successfully </p>
+          <div className="text-center font-semi-bold text-xl mt-12">
+           Sign Up Successfully
           </div>
         </div>
       </div>
