ea5895a10b64b3263a08ff8a871b4721fe14d247
Limin Chen <kathy@Williams-MacBook-Pro.local>
2023-06-21 21:41:27 +1000

    revise PopupPage 3rd

--- src/components/Modal/PopupPage.tsx
+++ src/components/Modal/PopupPage.tsx
@@ -12,25 +12,19 @@ const PopupPage: FC = () => {
   }
   return (
     <Container>
-      <div className="justify-center items-center">
-        <div className="border rounded-md w-96 h-80 bg-white max-auto text-center">
-          <div className="text-end">
-            <IconButton onClick={handleClose} className="text-black w-12 h-12">
-              <CloseRoundedIcon className="text-[32px]" />
-            </IconButton>
+        <div className="border rounded-md w-[494px] h-[405px] max-auto text-center">
+          <div className="text-end pr-5 pt-5 mb-8">
+              <CloseRoundedIcon onClick={handleClose} className="text-[32px] cursor-pointer" />
           </div>
           <div className="flex justify-center">
-            <div className="bg-[#0EA739] w-20 h-20 rounded-full">
-              <IconButton>
+              <IconButton className="bg-[#0EA739] w-[99px] h-[99px] rounded-full">
                 <CheckRounded className="text-white text-[64px]" />
               </IconButton>
-            </div>
           </div>
           <div className="text-center font-semi-bold text-xl mt-12">
            Sign Up Successfully
           </div>
         </div>
-      </div>
     </Container>
   )
 }
