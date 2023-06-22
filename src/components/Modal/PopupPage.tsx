30b3ebb20475a58a34a9bcc33c168ac0b61429ec
Limin Chen <kathy@Williams-MacBook-Pro.local>
2023-06-22 20:43:50 +1000

    add grey background and functions in PopupPage

--- src/components/Modal/PopupPage.tsx
+++ src/components/Modal/PopupPage.tsx
@@ -1,26 +1,16 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import CheckRounded from '@mui/icons-material/CheckRounded'
-import IconButton from '@mui/material/IconButton'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import Container from '@/layouts/Container'
 
-const PopupPage: FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(true)
-  const handleClose = (): void => {setIsOpen(false)}
-  if (!isOpen) {
-    return null
-  }
-  return (
-    <Container>
-      <div className="bg-[#03111B] bg-opacity-40 flex maxWidth h-[832px] items-center justify-center">
-        <div className="rounded-[10px] w-[494px] h-[405px] max-auto text-center bg-white shadow-md border-solid border-[#03111B] border-opacity-40">
-          <div className="text-end pr-5 pt-5 mb-8">
-              <CloseRoundedIcon onClick={handleClose} className="text-[32px] cursor-pointer" />
-          </div>
+// popup的入口文件是create-account /auth/signup/create-account，
+const PopupPage: FC<{onClose:()=>void}> = ({onClose}) =>
+( <Container>
+      <div className="fixed top-0 left-0 w-full h-full bg-[#03111B] bg-opacity-40 flex items-center justify-center z-50">
+        <div className="rounded-[10px] w-[494px] h-[405px] pt-5 text-end bg-white shadow-md border-solid border-[#03111B] border-opacity-40">
+              <CloseRoundedIcon onClick={onClose} className="cursor-pointer mb-10 mr-5" />
           <div className="flex justify-center">
-              <IconButton className="bg-[#0EA739] w-[99px] h-[99px] rounded-full">
-                <CheckRounded className="text-white text-[64px]" />
-              </IconButton>
+                <CheckRounded className="text-white bg-[#0EA739] w-[99px] h-[99px] rounded-full" />
           </div>
           <div className="text-center font-semi-bold text-xl mt-12">
            Sign Up Successfully
@@ -29,5 +19,5 @@ const PopupPage: FC = () => {
       </div>
     </Container>
   )
-}
+
 export default PopupPage
